b1a07afe3302de2544b2d2c79a114ce7aeff4dfc
raven4ever <wrtv23@gmail.com>
2023-01-18 12:02:37 +0200

    custom 403

--- cloudfront.tf
+++ cloudfront.tf
@@ -40,6 +40,12 @@ resource "aws_cloudfront_distribution" "storage_bucket_distribution" {
     response_page_path = "/404.html"
   }
 
+  custom_error_response {
+    error_code         = 403
+    response_code      = 404
+    response_page_path = "/404.html"
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
