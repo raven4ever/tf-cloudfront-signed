dad0919e7de65b85699829bd1ff1a1904f6e79b4
raven4ever <wrtv23@gmail.com>
2023-01-18 09:56:28 +0200

    enable encryption by default

--- main.tf
+++ main.tf
@@ -9,3 +9,14 @@ resource "aws_s3_bucket_acl" "storage_bucket_acl" {
   bucket = aws_s3_bucket.storage_bucket.id
   acl    = "private"
 }
+
+# Enable encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "storage_bucket_encryption" {
+  bucket = aws_s3_bucket.storage_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
