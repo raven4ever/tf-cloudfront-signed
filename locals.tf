90edc11113acc08d95d78d5819056aa428825df1
raven4ever <wrtv23@gmail.com>
2023-01-18 13:12:26 +0200

    format

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   distribution_https_url = format("http://%s", aws_cloudfront_distribution.storage_bucket_distribution.domain_name)
   kitty_https_url        = format("https://%s/kitty-01.jpeg", aws_cloudfront_distribution.storage_bucket_distribution.domain_name)
-  url_expiration_date    = formatdate("YYYY-MM-DD", timeadd(timestamp(), "48h"))
+  url_expiration_date    = formatdate("DD-MM-YYYY", timeadd(timestamp(), "48h"))
 }
